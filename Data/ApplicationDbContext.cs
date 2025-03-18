60e3b17105339b16e07e276a37ab72a4be9a31a3
dshkuma <91202041+dshkuma@users.noreply.github.com>
2025-03-18 16:11:34 +0530

    MyRazorApp

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentApp.Data
+{
+    using global::StudentApp.Models.StudentApp.Models;
+    using System.Collections.Generic;
+    using Microsoft.EntityFrameworkCore;
+    //using StudentApp.Models;
+
+    namespace StudentApp.Data
+    {
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+            public DbSet<Student> Students { get; set; }
+        }
+    }
+
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentApp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    namespace StudentApp.Models
+    {
+        public class Student
+        {
+            [Key]
+            public int StudentId { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+        }
+    }
+
+}
